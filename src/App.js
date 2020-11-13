01151140b6d9c69769c3e642db5731176dedd1b2
andrewtjong <andrewttj@yahoo.com>
2020-11-13 17:43:21 +0100

    changed start time

--- src/App.js
+++ src/App.js
@@ -1,34 +1,60 @@
-import React, {useState, useEffect, useRef} from "react"
-import useWordGame from "./useWordGame"
+import React,{useState, useEffect} from "react"
 
 function App() {
-  const {
-      textBoxRef, 
-      handleChange, 
-      text, 
-      isTimeRunning, 
-      timeRemaining, 
-      startGame, 
-      wordCount
-  } = useWordGame()
+  const STARTING_TIME = (15)
+  const [text, setText] = useState("")
+  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
+  const [isTimeRunning, setIsTimeRunning] = useState(false)
+  const [wordCount, setWordCount] = useState(0)
 
-    return (
+  function handleChange(event) {
+    const {value} = event.target
+    setText(value)
+  }
+
+  function calculateWordCount(text) {
+    const wordsArr = text.trim().split(" ")
+    return wordsArr.filter(word => word !== "").length
+  }
+
+  function beginGame() {
+    setIsTimeRunning(true)
+    setTimeRemaining(STARTING_TIME)
+    setText("")
+    setWordCount(0)
+  }
+
+  function endGame() {
+    setIsTimeRunning(false)
+    setWordCount(calculateWordCount(text))
+  }
+
+  useEffect(() => {
+    if (isTimeRunning && timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining(time => time -1)
+      }, 1000)
+    } else if (timeRemaining === 0 ){
+      endGame()
+    }
+  }, [timeRemaining, isTimeRunning])
+
+  return (
     <div>
-      <h1>How fast can you type?</h1>
-      <textarea
-      ref={textBoxRef}
+      <h1>How fast do you type?</h1>
+      <textarea 
       onChange={handleChange}
       value={text}
       disabled={!isTimeRunning}
       />
-      <h4>Time remaining: {timeRemaining}</h4>
+      <h3>Remaining Time: {timeRemaining}</h3>
       <button 
-      onClick={startGame}
+      onClick={beginGame}
       disabled={isTimeRunning}
       >Start</button>
-      <h1>Word Count: {wordCount}</h1>
+  <h3>Word Count: {wordCount}</h3>
     </div>
-    )
- }
+  )
+}
 
- export default App
+export default App 
